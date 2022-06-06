76b6e8b12ba0281c7720138b207aabbdf22d1405
Hunter Bertoson <Hunterkylebertoson@gmail.com>
2022-06-06 19:32:19 +0000

    Updated Files

--- src/utils/bin/commands.ts
+++ src/utils/bin/commands.ts
@@ -133,6 +133,12 @@ export const emacs = async (args?: string[]): Promise<string> => {
   return `you know what? just use vscode.`;
 };
 
+export const vscode = async (args: string[]): Promise<string> => {
+  window.open(`https://code.visualstudio.com/download`);
+
+  return 'Downloading VsCode....';
+};
+
 export const sudo = async (args?: string[]): Promise<string> => {
   window.open('https://www.youtube.com/watch?v=dQw4w9WgXcQ', '_blank'); // ...I'm sorry
   return `Permission denied: with little power comes... no responsibility? `;
@@ -141,14 +147,28 @@ export const sudo = async (args?: string[]): Promise<string> => {
 // Banner
 export const banner = (args?: string[]): string => {
   return `
-█████        ███                       ███████████                                   
-░░███        ░░░                       ░█░░░███░░░█                                   
- ░███        ████  █████ █████  ██████ ░   ░███  ░   ██████  ████████  █████████████  
- ░███       ░░███ ░░███ ░░███  ███░░███    ░███     ███░░███░░███░░███░░███░░███░░███ 
- ░███        ░███  ░███  ░███ ░███████     ░███    ░███████  ░███ ░░░  ░███ ░███ ░███ 
- ░███      █ ░███  ░░███ ███  ░███░░░      ░███    ░███░░░   ░███      ░███ ░███ ░███ 
- ███████████ █████  ░░█████   ░░██████     █████   ░░██████  █████     █████░███ █████
-░░░░░░░░░░░ ░░░░░    ░░░░░     ░░░░░░     ░░░░░     ░░░░░░  ░░░░░     ░░░░░ ░░░ ░░░░░ 
+888    888 888     888 888b    888 88888888888 8888888888 8888888b.                       
+888    888 888     888 8888b   888     888     888        888   Y88b                      
+888    888 888     888 88888b  888     888     888        888    888                      
+8888888888 888     888 888Y88b 888     888     8888888    888   d88P                      
+888    888 888     888 888 Y88b888     888     888        8888888P"                       
+888    888 888     888 888  Y88888     888     888        888 T88b                        
+888    888 Y88b. .d88P 888   Y8888     888     888        888  T88b                       
+888    888  "Y88888P"  888    Y888     888     8888888888 888   T88b                      
+                                                                                          
+                                                                                          
+                                                                                          
+888888b.   8888888888 8888888b. 88888888888 .d88888b.   .d8888b.   .d88888b.  888b    888 
+888  "88b  888        888   Y88b    888    d88P" "Y88b d88P  Y88b d88P" "Y88b 8888b   888 
+888  .88P  888        888    888    888    888     888 Y88b.      888     888 88888b  888 
+8888888K.  8888888    888   d88P    888    888     888  "Y888b.   888     888 888Y88b 888 
+888  "Y88b 888        8888888P"     888    888     888     "Y88b. 888     888 888 Y88b888 
+888    888 888        888 T88b      888    888     888       "888 888     888 888  Y88888 
+888   d88P 888        888  T88b     888    Y88b. .d88P Y88b  d88P Y88b. .d88P 888   Y8888 
+8888888P"  8888888888 888   T88b    888     "Y88888P"   "Y8888P"   "Y88888P"  888    Y888 
+                                                                                          
+                                                                                          
+                                                                                          
 
 Type 'help' to see the list of available commands.
 Type 'sumfetch' to display summary.
